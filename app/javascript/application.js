81a345c671d296d5dd3d30be1aff819262e9e496
fabio.nogueira.carmo <fabio.nogueira@soitic.com>
2022-01-18 16:45:20 -0300

    Insert javascripts

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+//= require stisla
+//= require scripts
+//= require custom
